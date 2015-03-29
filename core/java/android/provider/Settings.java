5163bfdae5758691244ffaccf3c3cedd9a5b684c
maxwen <max.weninger@gmail.com>
2015-03-29 18:39:55 +0200

    base: allow disable of screenshot shutter sound [1/2]
    [SahilSonar - POSP]
     - Forward port to android-12.0.0
    
    Change-Id: I47d52bba21170118af87d35376d81d7569587a2f

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5432,6 +5432,11 @@ public final class Settings {
         public static final String NETWORK_TRAFFIC_MODE = "network_traffic_mode";
 
         /**
+         * @hide
+         */
+        public static final String SCREENSHOT_SHUTTER_SOUND = "screenshot_shutter_sound";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
