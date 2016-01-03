55f4cbbb27995f0c2b611f833762adb03848ba50
maxwen <max.weninger@gmail.com>
2016-01-03 11:52:48 +0530

    SystemUI: auto brightness toggle improvements
    Change-Id: I99a39156f931943e98d539a07046698c16361d07

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -1197,6 +1197,7 @@ public class StatusBar extends SystemUI implements DemoMode,
                             .withDefault(this::createDefaultQSFragment)
                             .build());
             mBrightnessMirrorController = new BrightnessMirrorController(
+                    mContext,
                     mNotificationShadeWindowView,
                     mNotificationPanelViewController,
                     mNotificationShadeDepthControllerLazy.get(),
