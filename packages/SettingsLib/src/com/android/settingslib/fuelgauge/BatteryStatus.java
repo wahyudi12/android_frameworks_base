0256ad05bc4b7b16290a7ccfcb35e0ea0b403d24
Alexander Martinz <alex@amartinz.at>
2017-08-25 12:01:11 +0200

    BatteryService: Add support for oem fast charger detection
    Allows to indicate, whether a device is charged using a
    proprietary oem fast charge solution, which write their status
    to sysfs.
    
    The oem fast charge detection tries to be as generic as possible and is
    configured via overlays.
    
    Path to sysfs to read status
      - core/res/res/values/custom_config.xml
        - config_oemFastChargerStatusPath
    Value expected from read status (Defaults to "1" if not specified)
      - core/res/res/values/custom_config.xml
        - config_oemFastChargerStatusValue
    
    Change-Id: I6f3598a5a6a3efc76553261d2cf73094170d4110
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    Signed-off-by: Genkzsz11 <Genkzsz11@gmail.com>

--- packages/SettingsLib/src/com/android/settingslib/fuelgauge/BatteryStatus.java
+++ packages/SettingsLib/src/com/android/settingslib/fuelgauge/BatteryStatus.java
@@ -31,6 +31,7 @@ import static android.os.BatteryManager.EXTRA_STATUS;
 import static android.os.BatteryManager.EXTRA_DASH_CHARGER;
 import static android.os.BatteryManager.EXTRA_WARP_CHARGER;
 import static android.os.BatteryManager.EXTRA_VOOC_CHARGER;
+import static android.os.BatteryManager.EXTRA_OEM_FAST_CHARGER;
 
 import android.content.Context;
 import android.content.Intent;
@@ -65,11 +66,12 @@ public class BatteryStatus {
     public final boolean warpChargeStatus;
     public final boolean voocChargeStatus;
     public final boolean present;
+    public final boolean oemFastChargeStatus;
 
     public BatteryStatus(int status, int level, int plugged, int health,
             int maxChargingCurrent, int maxChargingVoltage,
             int maxChargingWattage, float temperature, boolean dashChargeStatus,
-            boolean warpChargeStatus, boolean voocChargeStatus, boolean present) {
+            boolean warpChargeStatus, boolean voocChargeStatus, boolean present, boolean oemFastChargeStatus) {
         this.status = status;
         this.level = level;
         this.plugged = plugged;
@@ -82,6 +84,7 @@ public class BatteryStatus {
         this.warpChargeStatus = warpChargeStatus;
         this.voocChargeStatus = voocChargeStatus;
         this.present = present;
+        this.oemFastChargeStatus = oemFastChargeStatus;
     }
 
     public BatteryStatus(Intent batteryChangedIntent) {
@@ -94,6 +97,7 @@ public class BatteryStatus {
         warpChargeStatus = batteryChangedIntent.getBooleanExtra(EXTRA_WARP_CHARGER, false);
         voocChargeStatus = batteryChangedIntent.getBooleanExtra(EXTRA_VOOC_CHARGER, false);
         present = batteryChangedIntent.getBooleanExtra(EXTRA_PRESENT, true);
+        oemFastChargeStatus = batteryChangedIntent.getBooleanExtra(EXTRA_OEM_FAST_CHARGER, false);
 
         final int maxChargingMicroAmp = batteryChangedIntent.getIntExtra(EXTRA_MAX_CHARGING_CURRENT,
                 -1);
@@ -172,6 +176,9 @@ public class BatteryStatus {
      * @return the charing speed
      */
     public final int getChargingSpeed(Context context) {
+        if (oemFastChargeStatus) {
+                return CHARGING_FAST;
+        }
         final int slowThreshold = context.getResources().getInteger(
                 R.integer.config_chargingSlowlyThreshold);
         final int fastThreshold = context.getResources().getInteger(
--- packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitor.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitor.java
@@ -1762,7 +1762,7 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener, Dumpab
         }
 
         // Take a guess at initial SIM state, battery status and PLMN until we get an update
-        mBatteryStatus = new BatteryStatus(BATTERY_STATUS_UNKNOWN, 100, 0, 0, 0, 0, 0, 0, false, false, false, true);
+        mBatteryStatus = new BatteryStatus(BATTERY_STATUS_UNKNOWN, 100, 0, 0, 0, 0, 0, 0, false, false, false, true, false);
 
         // Watch for interesting updates
         final IntentFilter filter = new IntentFilter();
@@ -2719,6 +2719,11 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener, Dumpab
 
         // Battery either showed up or disappeared
         if (wasPresent != nowPresent) {
+            return true;
+        }
+
+        // change in oem fast charging while plugged in
+        if (nowPluggedIn && current.oemFastChargeStatus != old.oemFastChargeStatus) {
             return true;
         }
 
