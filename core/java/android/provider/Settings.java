97e1a1151de538c4cabcb0f3b015f529727423fa
a.derendyaev <a.derendyaev@magdv.com>
2018-12-19 21:57:45 +0800

    SystemUI: runtime configurable audio panel location
    arrow-11.0 edits:
     - Port from tuner to settings observer
      Signed-off-by: ganeshi4u <ganeshvarmai4u@gmail.com>
    
    * Updates for eleven (LuK1337):
      * Disable on TVs
      * Simplify changes once again.
    
    * Updates for ten (sam3000):
      * Squash in tuner service settings reset blacklist.
      * Remove layout xml changes that aren't needed.
      * Apply slide in/out animations to landscape too
        (they work fine).
      * Few other simplifications.
    
    * Placement is now determined at runtime by a lineage setting.
      Use TunerService to track settings change.
      Various cleanup and fix formatting.
      Fixed left/right gravity on expand arrow and ringer.
      Fix commit author.
      (sam3000)
    
    This is a squashed and adapted commit of the following patches:
    
    ----
    
      From cf187bd54fbd807e16b0ac3593e839008efd302e Mon Sep 17 00:00:00 2001
      From: "a.derendyaev" <a.derendyaev@magdv.com>
      Date: Wed, 19 Dec 2018 21:57:45 +0800
      Subject: [PATCH] SystemUI: allow devices override audio panel location
    
      Some devices have volume buttons on left side and it not fancy if volume panel will be at right side.
      You can override panel location using overlay for SystemUI:
    
      <!-- Allow devices override audio panel location to the left side -->
      <bool name="config_audioPanelOnLeftSide">true</bool>
    
      Change-Id: I8a2302867010b0e6a02efa68df57e534ce7bbf46
      Signed-off-by: Jagrav Naik <jagravnaik0@gmail.com>
      Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>
    
    ----
    
      From 9d0ff1efd26961a4f14d0821f50ebe80c2a65ff9 Mon Sep 17 00:00:00 2001
      From: Alex Cruz <alex@dirtyunicorns.com>
      Date: Fri, 21 Dec 2018 22:08:52 -0600
      Subject: [PATCH] Volume panel: Do the same with less
    
      Use common volume_dialog layout for both left- and right-aligned panel
    
      [@TheDorkKnightRises - POSP]
      - Tweak padding values to make them uniform across both alignments
      - Remove redundant comment
    
      Change-Id: If41456f71ffd18466166e7b4120ff34d9e6f5a46
      Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
    
    ----
    
    Author: Sam Mortimer <sam@mortimer.me.uk>
    Date:   Sun Jun 9 13:33:05 2019 -0700
    
        SystemUI: Blacklist volume panel on left setting from tuner reset
    
        Change-Id: I47ddb3b818cfa3548fef42fa686d0991e2a35605
    
    ----
    
    Change-Id: Ie4e2a6660f76a45e4b8959471165655c0f06b18d
    
    extras
    
    Change-Id: If22c97eb1ce4cfb13396d182d90786f4b7acaee7
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6171,6 +6171,12 @@ public final class Settings {
         public static final String SCREENSHOT_SOUND = "screenshot_sound";
 
         /**
+         * Volume panel on left
+         * @hide
+         */
+        public static final String VOLUME_PANEL_ON_LEFT = "volume_panel_on_left";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
@@ -6546,6 +6552,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(STOCK_STATUSBAR_IN_HIDE);
             PRIVATE_SETTINGS.add(FORCE_FULLSCREEN_CUTOUT_APPS);
             PRIVATE_SETTINGS.add(INCALL_FEEDBACK_VIBRATE);
+            PRIVATE_SETTINGS.add(VOLUME_PANEL_ON_LEFT);
         }
 
         /**
