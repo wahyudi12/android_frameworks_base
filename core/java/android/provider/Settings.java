344e65f205db3ce3bb47408438660a5d6e057bed
Pranav Vashi <neobuddy89@gmail.com>
2020-01-05 02:29:56 +0530

    Add toggle to hide lock icon on lockscreen [1/2]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6620,6 +6620,12 @@ public final class Settings {
          */
         public static final String FOD_COLOR = "fod_color";
 
+        /**
+         * Whether to show lock icon on lockscreen
+         * @hide
+         */
+        public static final String LOCKSCREEN_LOCK_ICON = "lockscreen_lock_icon";
+
 	/**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
