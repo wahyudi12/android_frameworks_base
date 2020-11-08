bbd96ef3fa375e4e8e022d34e713e5f2e5ab838b
ezio84 <brabus84@gmail.com>
2020-11-08 16:19:23 +0100

    Show Ambient instead of lockscreen on wake gestures [1/2]
    Hey Google, please kang only good features by iOS -_-'
    
    Change-Id: I11b0ad51353550d585d4db7d30b983bf551a53a4
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5046,6 +5046,13 @@ public final class Settings {
          */
         public static final String NAVIGATION_BAR_ARROW_KEYS = "navigation_bar_menu_arrow_keys";
 
+        /** Whether to show ambient or lockscreen if AoD is disabled
+         * and we do a wake gesture like lift to wake or double tap
+         *
+         * @hide
+         */
+        public static final String AMBIENT_WAKE_GESTURES = "ambient_wake_gestures";
+
         /**
          * Whether user can swap navigation lateral keys.
          * <p>
