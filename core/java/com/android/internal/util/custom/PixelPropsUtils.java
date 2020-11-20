fe51c377d8f865ab1f50ff77b83fcaa7b3a40738
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-11-20 13:04:03 -0300

    base: PixelPropsUtils: Don't set build type/build debug props
    We are enforcing that on system/core
    
    Change-Id: I3b49e9cecc300c185c3e85343c8b7d323243347f
    Signed-off-by: Kunmun <kunmun.devroms@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- core/java/com/android/internal/util/custom/PixelPropsUtils.java
+++ core/java/com/android/internal/util/custom/PixelPropsUtils.java
@@ -80,11 +80,6 @@ public class PixelPropsUtils {
         propsToChange.put("PRODUCT", "redfin");
         propsToChange.put("MODEL", "Pixel 5");
         propsToChange.put("FINGERPRINT", "google/redfin/redfin:12/SQ1A.211205.008/7888514:user/release-keys");
-        propsToChange.put("IS_DEBUGGABLE", false);
-        propsToChange.put("IS_ENG", false);
-        propsToChange.put("IS_USERDEBUG", false);
-        propsToChange.put("IS_USER", true);
-        propsToChange.put("TYPE", "user");
     }
 
     public static void setProps(String packageName) {
