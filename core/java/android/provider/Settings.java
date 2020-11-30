ed32c7e9d3c3321060d7cba9bacd93245c47e558
Joey Huab <joey@evolution-x.org>
2020-12-01 06:50:16 +0900

    [SQUASH] SystemUI: FOD pressed colors [1/2]
    Includes the following:
    * https://github.com/Evolution-X/frameworks_base/commit/b70ac04e293c7a2eea2710a1a689630a74e70364
    * https://github.com/Evolution-X/frameworks_base/commit/6ecbadff3550c01682a15331f3ccb0abf614d845

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6551,6 +6551,12 @@ public final class Settings {
          */
         public static final String QS_CLOCK_GRAVITY = "clock_gravity";
 
+        /**
+         * FOD pressed color
+         * @hide
+         */
+        public static final String FOD_COLOR = "fod_color";
+
 	/**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
