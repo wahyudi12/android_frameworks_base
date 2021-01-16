3e7a654101faf70a721f6e0389e056cca616bc3c
spezi77 <spezi7713@gmx.net>
2021-01-16 20:31:43 +0100

    Ambient music v2: Allow for changing the music ticker position [1/2].
    This will add the music ticker to the ambient screen / AoD just like
    on Pixel devices. It will work with ezio's music ticker and now playing
    commits for Android R. Other part of the commit is in Settings.
    
    The core of the ambient music ticker uses the media detection logic from
    the KeyguardSliceProvider.
    
    [@spezi77] General notes:
    * Complete rewrite for Android R, partially based on the commits by @ezio84
      and other authors from Android Pie (see comment below).
    * Kept the indication clean, no marquee and no fancy background pill,
      closer to what Google implemented with the Pixel factory image (Android R).
    * Improved the NP indication play nice with ambient wake gestures.
      If you wake the device from sleep (lift or double-tap) in certain scenarios
      (when using ambient on wake instead of lockscreen while AoD is disabled),
      the "Now Playing" title is not displayed. In that case it is only displayed
      when you activate the lock screen and return to the ambient display.
      Ref. commit for the ambient on wake option:
      https://github.com/PixelDust-Eleven/android_frameworks_base/commit/75d7c4b54a204fa08794df9265bea926cabd3d6e
    
    Change-Id: I523c1d80108e936f596010209eb1a10939779de8
    
    ---
    
    Some parts are based in part on code from other authors. Unfortunately, when
    porting the feature to Android R, I couldn't insert these as individual commits,
    because I had to rewrite a lot of things.
    
    To let you know who also contributed, some mentions:
    ezio84, frap129, eldainosor and jhenrique09
    
    To mention some of the commits:
    * Port ambient Now Playing container from Pixel [stripped down version] https://github.com/pixeldust-project-caf/android_frameworks_base/commit/bf4c64575cbe2633be12493ea8f51f01fd05fc8e
    * Ambient display music ticker [1/2] https://github.com/ezio84/abc_frameworks_base/commit/b6ec18a72c9a944eb4e679f0239cc8d84865ea37
    * Ambient Music Ticker: make it pixel like https://github.com/pixeldust-project-caf/android_frameworks_base/commit/9cee2d4e20f57838b47abc89d5c81e3caa19651d
    * Improve ambient play layout on keyguard https://github.com/PixelExperience/frameworks_base/commit/55d32f370997cbd86b7d2bc626cada07306fa4c5#diff-a24e5bd63f05adbcd800f9daa0d7bf3a0fc762c0096eaef0962ae04b10b62696
    
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Change-Id: If49705681090ad8ff6a08e01f166461d99ef4d33

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -1200,7 +1200,7 @@ public class StatusBar extends SystemUI implements DemoMode,
         mAmbientIndicationContainer = mNotificationShadeWindowView.findViewById(
                 R.id.ambient_indication_container);
         if (mAmbientIndicationContainer != null) {
-            ((AmbientIndicationContainer) mAmbientIndicationContainer).initializeView(this);
+            ((AmbientIndicationContainer) mAmbientIndicationContainer).initializeView(this, mHandler);
         }
 
         mAutoHideController.setStatusBar(new AutoHideUiElement() {
@@ -3404,6 +3404,10 @@ public class StatusBar extends SystemUI implements DemoMode,
         mScreenPinningRequest.onConfigurationChanged();
     }
 
+    public NotificationLockscreenUserManager getNotificationLockscreenUserManager() {
+        return mLockscreenUserManager;
+    }
+
     /**
      * Notify the shade controller that the current user changed
      *
@@ -3983,6 +3987,14 @@ public class StatusBar extends SystemUI implements DemoMode,
         mNotificationPanelViewController.setDozing(mDozing, animate, mWakeUpTouchLocation);
         mPulseController.setDozing(mDozing);
         updateQsExpansionEnabled();
+
+        if (mAmbientIndicationContainer != null) {
+            ((AmbientIndicationContainer)mAmbientIndicationContainer)
+                    .updateDozingState(mDozing);
+        } else {
+            Log.d("StatusBar", "updateDozingState -> AmbientIndicationContainer null");
+        }
+
         Trace.endSection();
     }
 
@@ -4101,9 +4113,6 @@ public class StatusBar extends SystemUI implements DemoMode,
                         mStatusBarStateController.fromShadeLocked());
             }
             if (mStatusBarView != null) mStatusBarView.removePendingHideExpandedRunnables();
-            if (mAmbientIndicationContainer != null) {
-                mAmbientIndicationContainer.setVisibility(View.VISIBLE);
-            }
         } else {
             mKeyguardIndicationController.setVisible(false);
             if (mKeyguardUserSwitcher != null) {
@@ -4111,9 +4120,6 @@ public class StatusBar extends SystemUI implements DemoMode,
                         mStatusBarStateController.goingToFullShade() ||
                                 mState == StatusBarState.SHADE_LOCKED ||
                                 mStatusBarStateController.fromShadeLocked());
-            }
-            if (mAmbientIndicationContainer != null) {
-                mAmbientIndicationContainer.setVisibility(View.INVISIBLE);
             }
         }
         updateDozingState();
@@ -4123,6 +4129,13 @@ public class StatusBar extends SystemUI implements DemoMode,
         mPulseController.setKeyguardShowing(mState == StatusBarState.KEYGUARD);
         updateKeyguardState();
         ((StatusBarIconControllerImpl) mIconController).setKeyguardShowing(mState == StatusBarState.KEYGUARD);
+
+        if (mAmbientIndicationContainer != null) {
+            ((AmbientIndicationContainer)mAmbientIndicationContainer)
+                    .updateKeyguardState(mState == StatusBarState.KEYGUARD);
+        } else {
+            Log.d("StatusBar", "updateKeyguardState -> AmbientIndicationContainer null");
+        }
         Trace.endSection();
     }
 
