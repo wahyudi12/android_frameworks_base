a632539f00093a7b5e18965ee184d4cd1051fcf1
Rahul <rahul731999@gmail.com>
2021-04-22 21:53:20 +0530

    Introduce Octavi's lockscreen clock

--- packages/SystemUI/src/com/android/keyguard/clock/ClockManager.java
+++ packages/SystemUI/src/com/android/keyguard/clock/ClockManager.java
@@ -172,6 +172,8 @@ public final class ClockManager {
         addBuiltinClock(() -> new AndroidSClockController(res, layoutInflater, colorExtractor));
         addBuiltinClock(() -> new AndroidSDP3ClockController(res, layoutInflater, colorExtractor));
         addBuiltinClock(() -> new Fluidv2ClockController(res, layoutInflater, colorExtractor));
+        addBuiltinClock(() -> new OctaviDigitalClockController(res, layoutInflater, colorExtractor));
+        // Store the size of the display for generation of clock preview.
         DisplayMetrics dm = res.getDisplayMetrics();
         mWidth = dm.widthPixels;
         mHeight = dm.heightPixels;
