GrazieMaldini/du_frameworks_base

Introduce RGB accenter [1/2]
Signed-off-by: Genkzsz11 <Genkzsz11@gmail.com>

core/java/android/content/res/AccentUtils.java
@@ -1,0 +1,37 @@
+package android.content.res;
+
+import android.graphics.Color;
+import android.os.SystemProperties;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class AccentUtils {
+    private static final String TAG = "AccentUtils";
+
+
+    private static final String ACCENT_COLOR_PROP = "persist.sys.du.accent_color";
+
+    static boolean isResourceAccent(String resName) {
+        return resName.contains("accent_device_default_light")
+                || resName.contains("accent_device_default_dark");
+    }
+
+    public static int getNewAccentColor(int defaultColor) {
+        return getAccentColor(defaultColor, ACCENT_COLOR_PROP);
+    }
+
+    private static int getAccentColor(int defaultColor, String property) {
+        try {
+            String colorValue = SystemProperties.get(property, "-1");
+            return "-1".equals(colorValue)
+                    ? defaultColor
+                    : Color.parseColor("#" + colorValue);
+        } catch (Exception e) {
+            Log.e(TAG, "Failed to set accent: " + e.getMessage() +
+                    "\nSetting default: " + defaultColor);
+            return defaultColor;
+        }
+    }
+}

wahyudi12/android_frameworks_base

core/java/android/content/res/AccentUtils.java
@@ -1,0 +1,37 @@
+package android.content.res;
+
+import android.graphics.Color;
+import android.os.SystemProperties;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class AccentUtils {
+    private static final String TAG = "AccentUtils";
+
+
+    private static final String ACCENT_COLOR_PROP = "persist.sys.nad.accent_color";
+
+    static boolean isResourceAccent(String resName) {
+        return resName.contains("accent_device_default_light")
+                || resName.contains("accent_device_default_dark");
+    }
+
+    public static int getNewAccentColor(int defaultColor) {
+        return getAccentColor(defaultColor, ACCENT_COLOR_PROP);
+    }
+
+    private static int getAccentColor(int defaultColor, String property) {
+        try {
+            String colorValue = SystemProperties.get(property, "-1");
+            return "-1".equals(colorValue)
+                    ? defaultColor
+                    : Color.parseColor("#" + colorValue);
+        } catch (Exception e) {
+            Log.e(TAG, "Failed to set accent: " + e.getMessage() +
+                    "\nSetting default: " + defaultColor);
+            return defaultColor;
+        }
+    }
+}