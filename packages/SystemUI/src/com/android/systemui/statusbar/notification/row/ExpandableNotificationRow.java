6dd69b0795f335d3ee1283156ddd4463bf57bb90
kqixs <kl.kqixs@gmail.com>
2021-05-23 09:15:09 +0530

    Fix Statusbar Icon Colors on Lockscreen

--- packages/SystemUI/src/com/android/systemui/statusbar/notification/row/ExpandableNotificationRow.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/notification/row/ExpandableNotificationRow.java
@@ -626,7 +626,7 @@ public class ExpandableNotificationRow extends ActivatableNotificationView
                         getBackgroundColorWithoutTint());
             }
         }
-        expandedIcon.setStaticDrawableColor(color);
+        expandedIcon.setStaticDrawableColorNotif(color);
     }
 
     public void setAboveShelfChangedListener(AboveShelfChangedListener aboveShelfChangedListener) {
