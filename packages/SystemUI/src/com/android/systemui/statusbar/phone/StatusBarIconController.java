96898273cce06bbcf3e6fdea3c5cb8f9a85a2159
Genkzsz11 <Genkzsz11@gmail.com>
2021-06-07 14:40:08 +0700

    Revert "Network Traffic: remove statusbar animation"
    This reverts commit df5554ae1db4ed265972dbb517b832d51135cafc.

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconController.java
@@ -332,6 +332,7 @@ public interface StatusBarIconController {
 
         private NetworkTrafficSB onCreateNetworkTraffic(String slot) {
             NetworkTrafficSB view = new NetworkTrafficSB(mContext);
+            view.setPadding(4, 0, 4, 0);
             return view;
         }
 
