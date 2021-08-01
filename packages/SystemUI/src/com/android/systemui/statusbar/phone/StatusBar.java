983e6dec728df4f491da12f33dec9309ffd4d8ad
Kshitij Gupta <kshitijgm@gmail.com>
2021-08-01 19:40:17 +0900

    fixup! Custom Rounded Corner preference [1/2]
    neobuddy89
    * Simplified adaptation for A11.
    
    Revert "Custom Rounded Corner and Padding preferences [1/2] (WIP)"
    
    This reverts commit 193deb4f7db1d6340fd28f9edce793d15346bdab.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -2390,7 +2390,7 @@ public class StatusBar extends SystemUI implements DemoMode,
                     uri.equals(Settings.Secure.getUriFor("sysui_rounded_size"))) {
                 handleCutout(null);
             } else if (uri.equals(Settings.Secure.getUriFor(Settings.Secure.SYSUI_ROUNDED_FWVALS))) {
-                updateCorners();
+                updateRoundedFwvals();
             } else if (uri.equals(Settings.Secure.getUriFor(
                     Settings.Secure.FP_SWIPE_TO_DISMISS_NOTIFICATIONS))) {
                 setFpToDismissNotifications();
@@ -2410,7 +2410,7 @@ public class StatusBar extends SystemUI implements DemoMode,
             setPulseOnNewTracks();
             setScreenBrightnessMode();
             setChargingAnimation();
-            updateCorners();
+            updateRoundedFwvals();
             handleCutout(null);
             setFpToDismissNotifications();
             updateTicker();
@@ -2537,38 +2537,28 @@ public class StatusBar extends SystemUI implements DemoMode,
         setStatusBarStockOverlay(hideCutoutMode && statusBarStock);
     }
 
-    private void updateCorners() {
-        mSysuiRoundedFwvals = Settings.Secure.getIntForUser(mContext.getContentResolver(),
-                Settings.Secure.SYSUI_ROUNDED_FWVALS, 1,
-                UserHandle.USER_CURRENT) == 1;
+    private void updateRoundedFwvals() {
         if (mSysuiRoundedFwvals && !isCurrentRoundedSameAsFw()) {
             float density = Resources.getSystem().getDisplayMetrics().density;
             int resourceIdRadius = (int) mContext.getResources().getDimension(com.android.internal.R.dimen.rounded_corner_radius);
             Settings.Secure.putIntForUser(mContext.getContentResolver(),
                 Settings.Secure.SYSUI_ROUNDED_SIZE, (int) (resourceIdRadius / density), UserHandle.USER_CURRENT);
-            int resourceIdPadding = (int) mContext.getResources().getDimension(R.dimen.rounded_corner_content_padding);
-            Settings.Secure.putIntForUser(mContext.getContentResolver(),
-                Settings.Secure.SYSUI_ROUNDED_CONTENT_PADDING, (int) (resourceIdPadding / density), UserHandle.USER_CURRENT);
         }
     }
 
-    private boolean isCurrentRoundedSameAsFw() {
+    public boolean isCurrentRoundedSameAsFw() {
         float density = Resources.getSystem().getDisplayMetrics().density;
         // Resource IDs for framework properties
         int resourceIdRadius = (int) mContext.getResources().getDimension(com.android.internal.R.dimen.rounded_corner_radius);
-        int resourceIdPadding = (int) mContext.getResources().getDimension(R.dimen.rounded_corner_content_padding);
 
         // Values on framework resources
         int cornerRadiusRes = (int) (resourceIdRadius / density);
-        int contentPaddingRes = (int) (resourceIdPadding / density);
 
         // Values in Settings DBs
         int cornerRadius = Settings.Secure.getIntForUser(mContext.getContentResolver(),
                 Settings.Secure.SYSUI_ROUNDED_SIZE, cornerRadiusRes, UserHandle.USER_CURRENT);
-        int contentPadding = Settings.Secure.getIntForUser(mContext.getContentResolver(),
-                Settings.Secure.SYSUI_ROUNDED_CONTENT_PADDING, contentPaddingRes, UserHandle.USER_CURRENT);
 
-        return (cornerRadiusRes == cornerRadius) && (contentPaddingRes == contentPadding);
+        return (cornerRadiusRes == cornerRadius);
     }
 
     @VisibleForTesting
@@ -4392,7 +4382,6 @@ public class StatusBar extends SystemUI implements DemoMode,
         }
         haltTicker();
         haltLyricTicker();
-        updateCorners();
     }
 
     private void updateDozingState() {
