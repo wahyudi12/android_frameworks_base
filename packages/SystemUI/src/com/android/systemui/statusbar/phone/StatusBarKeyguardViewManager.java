4ab9c2ca711d8ee1ebea811804b7a32c2cc10122
NurKeinNeid <mralexman3000@gmail.com>
2021-08-10 21:22:10 +0200

    Revert "base: Allow hiding the lock icon on lockscreen [1/2]"
    This reverts commit 59ff9da4a54e78f1cbd3948d1758f10a5cfe8e0c.
    
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>
    Signed-off-by: Genkzsz11 <Genkzsz11@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarKeyguardViewManager.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarKeyguardViewManager.java
@@ -30,7 +30,6 @@ import android.content.Context;
 import android.content.res.ColorStateList;
 import android.os.Bundle;
 import android.os.SystemClock;
-import android.provider.Settings;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.ViewGroup;
@@ -168,7 +167,6 @@ public class StatusBarKeyguardViewManager implements RemoteInputController.Callb
     private boolean mPulsing;
     private boolean mGesturalNav;
     private boolean mIsDocked;
-    private boolean isHideLockIcon;
 
     protected boolean mFirstUpdate = true;
     protected boolean mLastShowing;
@@ -254,7 +252,7 @@ public class StatusBarKeyguardViewManager implements RemoteInputController.Callb
         mStatusBar = statusBar;
         mContainer = container;
         mLockIconContainer = lockIconContainer;
-        if (mLockIconContainer != null && !isHideLockIcon) {
+        if (mLockIconContainer != null) {
             mLastLockVisible = mLockIconContainer.getVisibility() == View.VISIBLE;
         }
         mBiometricUnlockController = biometricUnlockController;
@@ -342,9 +340,7 @@ public class StatusBarKeyguardViewManager implements RemoteInputController.Callb
         }
         boolean keyguardWithoutQs = mStatusBarStateController.getState() == StatusBarState.KEYGUARD
                 && !mNotificationPanelViewController.isQsExpanded();
-        isHideLockIcon = Settings.Secure.getInt(mContext.getContentResolver(),
-                Settings.Secure.HIDE_LOCKICON, 1) == 1;
-        boolean lockVisible = (mBouncer.isShowing() || keyguardWithoutQs) && !isHideLockIcon
+        boolean lockVisible = (mBouncer.isShowing() || keyguardWithoutQs)
                 && !mBouncer.isAnimatingAway() && !mKeyguardStateController.isKeyguardFadingAway();
 
         if (mLastLockVisible != lockVisible) {
@@ -849,8 +845,6 @@ public class StatusBarKeyguardViewManager implements RemoteInputController.Callb
         boolean bouncerInTransit = mBouncer.inTransit();
         boolean bouncerDismissible = !mBouncer.isFullscreenBouncer();
         boolean remoteInputActive = mRemoteInputActive;
-        isHideLockIcon = Settings.Secure.getInt(mContext.getContentResolver(),
-                Settings.Secure.HIDE_LOCKICON, 1) == 1;
 
         if (mLockScreenBlur > 0f && mBlurUtils.supportsBlursOnWindows()) {
             if (showing && !occluded) {
