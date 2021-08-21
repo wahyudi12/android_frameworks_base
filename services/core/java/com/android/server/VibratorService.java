696fd300140c12adb1958857b884d17ad3d46c5e
mdeejay <mdjrussia@gmail.com>
2021-08-21 13:02:47 +0300

    Revert "[DO NOT MERGE] VibratorService: bypass AppOps check"
    This reverts commit 33b569b551ad437c25e8dcd3cd1ee5febfc99cf5.
    
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Change-Id: Ie85a4963ca687064eaeb0c6c7db0d86d87bebbe7

--- services/core/java/com/android/server/VibratorService.java
+++ services/core/java/com/android/server/VibratorService.java
@@ -1104,8 +1104,6 @@ public class VibratorService extends IVibratorService.Stub
             return false;
         }
 
-        // XXX: AppOps is randomly rejecting vibration calls from SystemUI
-        /*
         final int mode = getAppOpMode(vib.uid, vib.opPkg, vib.attrs);
         if (mode != AppOpsManager.MODE_ALLOWED) {
             if (mode == AppOpsManager.MODE_ERRORED) {
@@ -1115,7 +1113,6 @@ public class VibratorService extends IVibratorService.Stub
             }
             return false;
         }
-        */
 
         return true;
     }
