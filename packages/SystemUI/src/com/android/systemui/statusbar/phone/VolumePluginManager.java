8af1f36f509fa3510e8c294f5472731f961c16b9
703joko <703joko@gmail.com>
2021-08-29 05:51:44 +0000

    Volume : Add Nusantara X [1/3]

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/VolumePluginManager.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/VolumePluginManager.java
@@ -78,6 +78,7 @@ public class VolumePluginManager extends BroadcastReceiver {
         "com.kangos.plugin.volume.miui",
         "co.potatoproject.plugin.volume.p404",
         "co.potatoproject.plugin.volume.ssos",
+        "co.potatoproject.plugin.volume.jr",
     };
 
     private PluginPrefs mPluginPrefs;
